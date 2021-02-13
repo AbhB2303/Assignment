994c02f225fc3405f754d7c4ad37b00cdc4b67e7
abhinavbatra <abhinavbatra2303@gmail.com>
2021-02-12 20:23:23 -0500

    Final session

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         int[] InputValues = InputReader(); //read input from files and assign to array
 
