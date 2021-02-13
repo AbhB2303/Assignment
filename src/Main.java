fba19a75f2f933047c4fd35295933112c836b5fa
abhinavbatra <abhinavbatra2303@gmail.com>
2021-02-12 22:48:36 -0500

    Final session: Last changes

--- src/Main.java
+++ src/Main.java
@@ -5,53 +5,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int[] InputValues = InputReader(); //read input from files and assign to array
-
-        Merge merge = new Merge(InputValues, 1); //create merge object and assign InputValues by passing through constructor
+        int size = 8; //value assigned for size of array to be read
+        int[] InputValues = new int[size]; //read input from files and assign to array
+        OutputToFile(null, false); //clear current output.txt
+        InputValues = InputReader(size); //read input of length size and save in inputvalues array
+        Merge merge = new Merge(InputValues); //create merge object and assign InputValues by passing through constructor
         merge.run(); //run the thread
-
 
         int[] OutputValues = merge.getArray(); //grab final sorted array and set it to output array to be written to text file
 
-        //output sorted values to console
-        for(int i = 0; i < OutputValues.length; i++){
-            System.out.println(OutputValues[i]);
-        }
-
-       // OutputToFile(OutputValues);//writes output to output.txt
+        OutputToFile(OutputValues, true);//writes output to output.txt
     }
 
 
-
-    public static int[] InputReader() //function to read file input
+    public static int[] InputReader(int size) //function to read file input
     {
-        int[] InputValues = new int[8];
-        try{ //read inputs and assign to array
+        int[] InputValues = new int[size];
+        try { //read inputs and assign to array
             File file = new File("Input.txt"); //define new file
             Scanner scan = new Scanner(file); //scan file
-            for(int i = 0; i < InputValues.length; i++){ //read through each line of file
+            for (int i = 0; i < InputValues.length; i++) { //read through each line of file
                 InputValues[i] = scan.nextInt();
             }
 
 
-        } catch(IOException e) {
+        } catch (IOException e) {
             System.out.println("An error occurred");
             e.printStackTrace();
         }
         return InputValues; //return values read from the input
     }
 
-    public static void OutputToFile(int[] arr) //function to write file output given an array to write
+
+    public static void OutputToFile(int[] arr, boolean append) //function to write file output given an array to write
     {
         try { //write to output file
-            FileWriter WriteToOutput = new FileWriter("Output.txt"); //create file writer
-
-            for (int i = 0; i < arr.length; i++) { //write each array value into file line by line
-                WriteToOutput.write(arr[i] + "\n");
+            FileWriter WriteToOutput = new FileWriter("Output.txt", append); //create file writer
+            if (arr != null) {
+                for (int i = 0; i < arr.length; i++) { //write each array value into file line by line
+                    WriteToOutput.write(arr[i] + "\n");
+                }
             }
-
+            else
+            {
+                WriteToOutput.write("");
+            }
             WriteToOutput.close(); //close out the file writer
-            System.out.println("Wrote to the file"); //confirm that the file was written to
+            //System.out.println("Wrote to the file"); //confirm that the file was written to
         } catch (IOException e) {
             System.out.println("Error");
             e.printStackTrace();
--- src/Merge.java
+++ src/Merge.java
@@ -3,8 +3,6 @@ import java.io.IOException;
 
 public class Merge implements Runnable {
     public int[] array;//input array
-    private int count;
-
     //getter and setter for the array
     public int[] getArray() {
         return array;
@@ -16,19 +14,15 @@ public class Merge implements Runnable {
     //constructors:
 
 
-    public Merge(int[] input, int c) {//called at first instance of merge being created
-        this.count = c;
-        //System.out.println(c);
+    public Merge(int[] input) {//called at first instance of merge being created
         this.array = new int[input.length];
         array = input;
     }
 
     //constructor to merge together sorted partitions
     //called after sorting is complete
-    public Merge(int[] input1, int[] input2, int c) {  //mid is the start of the second array
-        this.count = c;
-        System.out.println(c);
-        OutputToFile(c);
+    public Merge(int[] input1, int[] input2) {  //mid is the start of the second array
+
         array = new int[input1.length+input2.length]; //set array to new array of size of input1 and input2 combined
         int mid = input1.length; //define a mid for the new array
 
@@ -42,7 +36,7 @@ public class Merge implements Runnable {
 
         if (array[mid - 1] <= array[mid]) { //check if merging needs to occur
             // (if rightmost value in left partition >= leftmost part of second partition, the array is already sorted)
-            return;
+           return;
         }
 
         //set values for pointers of the two partition
@@ -60,7 +54,7 @@ public class Merge implements Runnable {
                 aux[auxIndex++] = array[i++]; //next value in auxiliary array
             } else
             {
-                aux[auxIndex++] = array[j++];
+                aux[auxIndex++] = array[j++]; //
             }
         }
 
@@ -72,8 +66,12 @@ public class Merge implements Runnable {
             aux[auxIndex++] = array[k];
         }
 
+
+
         for (int k = 0; k < aux.length; k++) { //copy auxiliary back into the original array
+
             array[k] = aux[k];
+
         }
     }
 
@@ -89,33 +87,40 @@ public class Merge implements Runnable {
     }
 
     private void sort() { //sorting function to be called from runnable
+
         int[] arr; //define a new array to take on the sliced arrays
         if(array.length <= 1) //if array size is one or the array is empty, return; base case for sort
         {
             return;
         }
         int mid = (array.length)/ 2; //define new mid for an array of size bigger than 1
-
-        arr = getArraySlice(0, mid); //slice the left side of the array to be send into the left thread
-        Merge left =  new Merge(arr, count++); //create merge object for left partition
-        Thread t1 = new Thread(left); //instantiate thread for the left partition
-        t1.start(); //start the sort of the left partition
+        arr = getArraySlice(0, mid); //slice the left side of the array to be sent into the left thread
+        Merge left =  new Merge(arr); //create merge object for left partition
+        Thread leftThread = new Thread(left); //instantiate thread for the left partition
+        leftThread.start(); //start the sort of the left partition
+        OutputToFile("New thread started: " + leftThread.getName());
 
         //repeat above process for the right hand side of the array
         arr = getArraySlice(mid, array.length);
-        Merge right = new Merge(arr, count++);
-        Thread t2 = new Thread(right);
-        t2.start();
+        Merge right = new Merge(arr);
+        Thread rightThread = new Thread(right);
+        rightThread.start();
+        OutputToFile("New thread started: " + rightThread.getName());
 
         //join threads when fully sorted
         try {
-            t1.join();
-            t2.join();
+
+            leftThread.join();
+            OutputToFile("Finish Thread: " + leftThread.getName());
+            rightThread.join();
+            OutputToFile("Finish Thread: " + rightThread.getName());
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        Merge combine = new Merge(left.getArray(), right.getArray(), count++); //instantiate merge constructor to implement merge
+        Merge combine = new Merge(left.getArray(), right.getArray()); //instantiate merge constructor to implement merge
         array = combine.getArray(); //assign array the final sorted and merged array to be sent to output text file
 
     }
@@ -124,23 +129,24 @@ public class Merge implements Runnable {
 
     @Override
     public void run(){
-        sort();
 
+        sort();
 
     }
 
-    public static void OutputToFile(int Output) //function to write file output given an array to write
+    public void OutputToFile(String OutputString) //function to write file output given an array to write
     {
         try { //write to output file
-            FileWriter WriteToOutput = new FileWriter("Output.txt"); //create file writer
+            FileWriter WriteToOutput = new FileWriter("Output.txt", true); //create file writer
+                WriteToOutput.write(OutputString + "\n");
 
-
-                WriteToOutput.write(Output + "\n");
-            //WriteToOutput.close(); //close out the file writer
+            WriteToOutput.close(); //close out the file writer
             //System.out.println("Wrote to the file"); //confirm that the file was written to
         } catch (IOException e) {
             System.out.println("Error");
             e.printStackTrace();
         }
     }
+
+
 }
